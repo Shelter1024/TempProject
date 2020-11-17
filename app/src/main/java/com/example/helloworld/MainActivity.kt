406c0884ba148f8c231e1dcd8a67c6dbfba896c0
xietian <xietian@021.com>
2020-11-17 11:28:18 +0800

    test recent tasks

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,17 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import java.util.concurrent.Executor
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val list = listOf("a", "b", "c")
+        println("Shelter: " + list.indices)
+
+        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+        fruits.filter { it.startsWith("a") }
+                .sortedBy { it }
+                .map { it.toUpperCase(Locale.CHINA) }
+                .forEach { println(it) }
     }
 
     fun startActivity1(view: View?) {
         val intent = Intent(this, SecondActivity::class.java)
+        //在近期任务列表显示单个app的多个activity
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
         startActivity(intent)
     }
 

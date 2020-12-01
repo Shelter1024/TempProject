4ec609c364a10cf4ec7660baf23eacd725520b0d
xietian <xietian@021.com>
2020-12-01 11:45:58 +0800

    show settled data

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,12 +2,31 @@ package com.example.helloworld
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,26 +34,49 @@ class MainActivity : AppCompatActivity() {
         val list = listOf("a", "b", "c")
         println("Shelter: " + list.indices)
 
-        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
-        fruits.filter { it.startsWith("a") }
-                .sortedBy { it }
-                .map { it.toUpperCase(Locale.CHINA) }
-                .forEach { println(it) }
+        forecast_list.layoutManager
 
-        findViewById<TextView>(R.id.text).text = "close an issue"
+//        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+//        fruits.filter { it.startsWith("a") }
+//                .sortedBy { it }
+//                .map { it.toUpperCase(Locale.CHINA) }
+//                .forEach { println(it) }
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        val recyclerView: RecyclerView = find(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+
+        val person = Person()
+        Log.i("TAG", "onCreate: person.name = " + person.name)
+        person.name = "Panny"
+        Log.i("TAG", "onCreate: person.name = " + person.name)
+        val contentGroup = findViewById<ViewGroup>(android.R.id.content)
+        contentGroup[2].visibility = View.GONE
+
+        val fragment = Fragment();
+        fragment.toast("")
+
+        //调换参数顺序
+        niceToast(tag = "Shelter", message = "调换参数顺序")
+
     }
 
-    fun startActivity1(view: View?) {
-        val intent = Intent(this, SecondActivity::class.java)
-        //在近期任务列表显示单个app的多个activity
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-        startActivity(intent)
+    operator fun ViewGroup.get(position: Int): View = getChildAt(position)
+
+    fun Fragment.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(getActivity(), message, duration).show()
     }
 
-    fun startActivity2(view: View?) {
-
+    override fun onResume() {
+        super.onResume()
+        niceToast(message = "onResume")
     }
+
+    fun niceToast(message: String, tag: String = javaClass.simpleName, length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, "[$tag]$message", length).show()
+    }
+
+    fun add(x: Int, y: Int) = x + y
 
 
 }
---
+++ app/src/main/java/com/example/helloworld/Person.kt
@@ -0,0 +1,13 @@
+package com.example.helloworld
+
+/**
+ * @author: Shelter
+ * Create time: 2020/11/30, 17:03.
+ */
+class Person {
+    var name : String = "Shelter"
+    get() = field.toUpperCase()
+    set(value) {
+        field = "Name:$value"
+    }
+}
